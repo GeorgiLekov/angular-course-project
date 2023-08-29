08c22bd4df79c4f694ceb49c5285b65ceb7c3528
georgi-lekov-63 <georgi.v.lekov@gmail.com>
2023-08-29 08:22:18 +0300

    Add calls to the backend and service for fetching/overriding recipes, add resolver service to app router module to fetch recipes upon reaching that path.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,28 +2,35 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
-import {Subject} from "rxjs";
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeService {
-  recipesChanged = new Subject<Recipe[]>()
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Schnitzel',
-      'This is simply a test',
-      'https://media.istockphoto.com/id/603258520/photo/schnitzel-and-fried-potatoes.jpg?s=2048x2048&w=is&k=20&c=PNJrHyiCF16b_L3g_eeIjI6ULowqi8nd0vQNjm5_Q-o=',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Burger',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
-      [new Ingredient('Bun', 2), new Ingredient('Meat', 1)]
-    ),
-  ];
+  recipesChanged = new Subject<Recipe[]>();
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Schnitzel',
+  //     'This is simply a test',
+  //     'https://media.istockphoto.com/id/603258520/photo/schnitzel-and-fried-potatoes.jpg?s=2048x2048&w=is&k=20&c=PNJrHyiCF16b_L3g_eeIjI6ULowqi8nd0vQNjm5_Q-o=',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Burger',
+  //     'This is simply a test',
+  //     'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
+  //     [new Ingredient('Bun', 2), new Ingredient('Meat', 1)]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
   getRecipes() {
     return [...this.recipes];
   }
@@ -36,15 +43,15 @@ export class RecipeService {
     this.slService.addIngredients(ingredients);
   }
 
-  addRecipe(recipe: Recipe){
+  addRecipe(recipe: Recipe) {
     this.recipes.push(recipe);
     this.recipesChanged.next(this.recipes.slice());
   }
-  updateRecipe(index: number, newRecipe: Recipe){
+  updateRecipe(index: number, newRecipe: Recipe) {
     this.recipes[index] = newRecipe;
     this.recipesChanged.next(this.recipes.slice());
   }
-  deleteRecipe(index: number){
+  deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
     this.recipesChanged.next(this.recipes.slice());
   }
